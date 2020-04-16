74f17be80298196a7672c99648eaef1c935e04b4
venkatguduru88 <63272913+venkatguduru88@users.noreply.github.com>
2020-04-17 00:22:58 +0530

    Update KnightMinMoves.java

--- KnightMinMoves.java
+++ KnightMinMoves.java
@@ -1,5 +1,9 @@
 package com.test.rollstring;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 //Java program to find number of possible moves of knight 
 public class KnightMinMoves {
 
@@ -9,40 +13,55 @@ public class KnightMinMoves {
 		int X[] = { 2, 1, -1, -2, -2, -1, 1, 2 };
 		int Y[] = { 1, 2, 2, 1, -1, -2, -2, -1 };
 
-		int count = 0;
+		List<Integer> count = new ArrayList<>();
 
-		if (start_row >= 0 && star_col >= 0 && end_row < n && end_col < n) {
+		if (start_row < 0 && star_col < 0 && end_row > n && end_col > n) {
 			return -1;
-		}
-
-		int diff_row = start_row - end_row;
-		int diff_col = star_col - end_col;
-
-		while (diff_col <= 0) {
-			diff_col = diff_col / 2;
-			// Y.i
 		}
 
 		// Check if each possible move is valid or not
 		for (int i = 0; i < 8; i++) {
 			// Position of knight after move
-			int x = start_row + X[i];
-			int y = star_col + Y[i];
-
-			// count valid moves
-			if (x >= 0 && y >= 0 && x < n && y < n) {
-				System.out.println(X[i] + "," + Y[i]);
-				count++;
+			int c = 1;
+			int x = start_row;
+			int y = star_col;
+			x = x + X[i];
+			y = y + Y[i];
+			if (x == end_row && y == end_col) {
+				if (x >= 0 && y >= 0 && x < n && y < n) {
+					count.add(i);
+				}
 			}
-		}
 
-		// Return number of possible moves
-		return count;
+			for (int j = 0; j < 8; j++) {
+				// int cc=c;
+				int xx = x;
+				int yy = y;
+				xx = xx + X[j];
+				yy = yy + Y[j];
+				if (xx == end_row && yy == end_col) {
+					if (x >= 0 && y >= 0 && x < n && y < n) {
+						count.add(c + 1);
+						break;
+					}
+
+				}
+
+			}
+
+		}
+		Collections.sort(count);
+		int minmoves = -1;
+		if (count.size() >= 1) {
+			minmoves = count.get(0);
+		}
+		System.out.println(count);
+		return minmoves;
 	}
 
 	// Driver program to check findPossibleMoves()
 	public static void main(String[] args) {
 
-		System.out.println(findMinMoves(4, 0, 1, 4, 8));
+		System.out.println(findMinMoves(10, 4, 4, 4, 8));
 	}
 }
